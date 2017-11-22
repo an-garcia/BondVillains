30b4644c85b6e4e024781e4b5b4e97b285f4c861
Angel Newton <xanewton@gmail.com>
2017-11-22 17:48:14 -0500

    Add a TabViewController to show collection and table modes.

---
+++ BondVillains/VillainCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  VillainCollectionViewCell.swift
+//  BondVillains
+//
+//  Created by xengar on 2017-11-22.
+//  Copyright © 2017 xengar. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - VillainCollectionViewCell: UICollectionViewCell
+class VillainCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var villainImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+}
+
